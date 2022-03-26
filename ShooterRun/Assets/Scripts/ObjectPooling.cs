5e8e62c739e1d91bdb9e077967085cfae69bb83b
NurettinArabaci <nurettinarabaci@gmail.com>
2022-03-26 15:53:44 +0300

    Add ObjectPool
    ObjectPool scripti yazıldı

---
+++ ShooterRun/Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    [System.Serializable]
+
+    public class Pool
+    {
+        public string objectTag;
+        public int size;
+        public GameObject prefab;
+        
+    }
+
+    public static ObjectPooling Instance;
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDict;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        poolDict = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool item in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < item.size; i++)
+            {
+                GameObject obj = Instantiate(item.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDict.Add(item.objectTag, objectPool);
+        }
+    }
+    public GameObject SpawnObject(string tag, Vector3 pos, Quaternion rot)
+    {
+        if (poolDict[tag].Count <= 1)
+        {
+            GameObject objectSpawn = poolDict[tag].Dequeue();
+            GameObject obj = Instantiate(objectSpawn);
+            obj.SetActive(false);
+            poolDict[tag].Enqueue(obj);
+            objectSpawn.SetActive(true);
+            objectSpawn.transform.position = pos;
+            objectSpawn.transform.rotation = rot;
+            return objectSpawn;
+        }
+
+        else
+        {
+            GameObject objectSpawn = poolDict[tag].Dequeue();
+            objectSpawn.SetActive(true);
+            objectSpawn.transform.position = pos;
+            objectSpawn.transform.rotation = rot;
+            //poolDict[tag].Enqueue(objectToSpawn);
+            return objectSpawn;
+        }
+
+    }
+    public void BackToPool(GameObject objectToEnqueu, string tag)
+    {
+        objectToEnqueu.SetActive(false);
+        poolDict[tag].Enqueue(objectToEnqueu);
+    }
+
+}
